4f99715ece5eeb2192148cee574fd7d063309b6f
Thalles Izoldino <80859038+thalles-victor@users.noreply.github.com>
2021-11-28 10:05:19 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+"""
+  instalação: $ pip3 install googletrans
+
+  No caso, foi usado a versão 3.1.0a0, para
+  usar está ou outra, coloque o operador < == >
+    exp: pip3 install googletrans==3.1.0a0
+                                                                  
+
+  Obs: O VSCode não reconheceu a biblioteca, porém ele
+  funcionou escrevendo da maneira correda as linhas de código.
+"""
+
+from googletrans import Translator
+translator = Translator()
+
+"""
+  .translate().text -> tradução mais usada
+"""
+print(
+  translator.translate('dog', src='en', dest='pt').text
+)  #  -> cão
+
+
+
+"""
+  .translate().extra_data -> Um conjundo de informações
+  como, verbos, adjetivos... exemplos de uso...
+"""
+
+print(
+  translator.translate('Like', src='en', dest='pt').extra_data
+) #  -> {'translation': [['Gostar', 'Like', None, None, 10]], 'all-translations': [['verbo', ['gostar', 'desejar', ...
+
+"""
+  .detect() -> Detecta o idioma de algum texto.
+    
+    
+"""
+
+print(
+  translator.detect('rapariga') 
+) # -> Detected(lang=pt, confidence=1)
+
+print(
+  translator.detect('Agora quero que me retorne apénas o idioma').lang # Usar o .lang para retornar o idioma 
+) #  -> pt
